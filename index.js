4129bac9a3be353af03b98bc1cbdf78eb79c4b78
NilsPvR <nilsundgrk@gmx.de>
2021-06-20 17:41:14 +0200

    New command and highlight input or user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const { prefix } = require('./config.json');
+const { prefix, colors } = require('./config.json');
 
 const Discord = require('discord.js');
 
@@ -20,6 +20,7 @@ const client = new Discord.Client({
 
 const { MessageEmbed } = require('discord.js');
 const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+const { getMember } = require('./util/getMember');
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -29,7 +30,7 @@ client.on('message', async message => {
 	if (message.author.bot || message.channel.type == 'dm') return;
 
 	if (message.content.toLowerCase() == 's!boosters') {
-		message.channel.send(`<@${message.author.id}>, try using \`${prefix}boosters\` for now.`);
+		message.channel.send(`<@${message.author.id}>, try using \`${prefix}boosters\` or \`${prefix}booster\` for now.`);
 	}
 
 	const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
@@ -57,6 +58,7 @@ client.on('message', async message => {
 			const boostMembers = members.filter(mem => mem.premiumSinceTimestamp) // sort all boosters by timestamp of when they started bossting
 				.sort((aMem, bMem) => aMem.premiumSinceTimestamp - bMem.premiumSinceTimestamp);
 
+			const inputtedMember = await getMember(message, args.join(' '), members);
 
 			let index = 1;
 			const fieldArr = [];
@@ -65,9 +67,13 @@ client.on('message', async message => {
 				let boostersMsg20 = '';
 				for(let i = 0; i < 20; i++) {
 					const boostmem = boostMembers.first(); // get first member
-					// add a space to mark the message author
-					if (message.member.id == boostmem.id) boostersMsg20 += `\n\u2000${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
+
+					// look for the specified member
+					if (inputtedMember?.id == boostmem.id) boostersMsg20 += `\n\u2000${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
+					// add a space to mark the message author, if noone else defined
+					else if (message.member.id == boostmem.id && !inputtedMember) boostersMsg20 += `\n\u2000${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
 					else boostersMsg20 += `\n${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
+
 					boostMembers.delete(boostMembers.firstKey()); // delete the member
 				}
 
@@ -82,8 +88,10 @@ client.on('message', async message => {
 			const size = boostMembers.size;
 			for(let i = 0; i < size; i++) { // iterate over the last members
 				const boostmem = boostMembers.first(); // get first member
-				// add a space to mark the message author
-				if (message.member.id == boostmem.id) boostersMsg20 += `\n\u2000${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
+				// look for the specified member
+				if (inputtedMember?.id == boostmem.id) boostersMsg20 += `\n\u2000${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
+				// add a space to mark the message author, if noone else defined
+				else if (message.member.id == boostmem.id && !inputtedMember) boostersMsg20 += `\n\u2000${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
 				else boostersMsg20 += `\n${index++}. **${boostmem.displayName}** - (${boostmem.premiumSince.toLocaleDateString('en-US')})`;
 				boostMembers.delete(boostMembers.firstKey()); // delete the member
 			}
@@ -93,21 +101,61 @@ client.on('message', async message => {
 				value: boostersMsg20,
 			});
 
+			for (let i = 0; i < fieldArr.length; i++) { // check all fields
+				if (fieldArr[i].value?.charAt(1) == '\u2000') { // if the first member in the field is supposed to be intented
+					fieldArr[i].value = '່' + fieldArr[i].value.substring(1); // replace \n with special char so discord doesn't remove the space \u2000
+				}
+			}
+
 			if (fieldArr.length > 25) { // remove all fields that are more then 25
 				fieldArr.splice(25, fieldArr.length - 25);
 			}
 			const description = (fieldArr.length > 25) ? 'It seems like there are more then 500 boosters o.O. I am currently unable to display all boosters therefore only the first 500 boosters will be displayed' : '';
 
 			return message.channel.send(new MessageEmbed()
-				.setColor('F697FF')
+				.setColor(colors.pink)
 				.setTitle(`Nitro Boosters in ${message.channel.guild.name}`)
 
 				.setDescription(description)
 				.addFields(fieldArr))
 				.catch(console.error);
 		}
+		if (command == 'booster') {
+			const members = await message.guild.members.fetch();
+
+			const boostMembers = members.filter(mem => mem.premiumSinceTimestamp) // sort all boosters by timestamp of when they started bossting
+				.sort((aMem, bMem) => aMem.premiumSinceTimestamp - bMem.premiumSinceTimestamp)
+				.map(member => member);
+
+			let inputtedMember;
+			if (!args[0]) {
+				inputtedMember = message.member;
+			}
+			else {
+				inputtedMember = await getMember(message, args.join(' '), members);
+			}
+			if (!inputtedMember) return;
+
+			const boostingPlace = boostMembers.findIndex(mem => mem.id == inputtedMember.id) + 1;
+			if (boostingPlace === -1 || boostingPlace === 0) {
+				return message.channel.send(new MessageEmbed()
+					.setColor(colors.red)
+
+					.setDescription((inputtedMember.id == message.member) ? 'You are currently not boosting the server!' : `\`${args.join(' ')}\` is currently not boosting the server!`));
+			}
+
+
+			return message.channel.send(new MessageEmbed()
+				.setColor(colors.pink)
+				.setTitle('Boosting place')
+
+				.setDescription(`<@${inputtedMember.id}> is currently the **${boostingPlace}.** booster on this server!`)
+				.setFooter(`${inputtedMember.user.tag} - ${inputtedMember.id}`));
+
+
+		}
 		else {
-			message.channel.send(`The only command I currently provide is \`${prefix}boosters\`!`);
+			message.channel.send(`The only commands I currently provide are \`${prefix}boosters [member]\` and \`${prefix}booster [member]\`!\n\nThe \`[member]\` is optional.`);
 		}
 	}
 	catch(error) {
---
+++ util/getMember.js
@@ -0,0 +1,74 @@
+const { colors } = require('../config.json');
+
+// only allow id's or mentions | (id)
+const idregex = new RegExp(/^(?:<@!?)?(\d{18})>?$/i);
+// only allow discord tags | (tag)
+const tagregex = new RegExp(/(^.{2,}#\d{4})$/i);
+
+async function fgetMember(message, arg, members) {
+	if (message.channel.type === 'dm') return;
+	// get the id from mention or just plain id, check for 'y' at the end
+	const idMatches = arg.match(idregex);
+	const tagMatches = arg.match(tagregex);
+
+	if (!idMatches) { // search by name
+		let foundMember;
+
+		if (tagMatches) {
+			foundMember = members.find(mem => mem.user.tag == tagMatches[1]); // if they use Metzok#6146yes -> get the tag like this
+			foundMember = foundMember ?? members.find(mem => mem.user.tag.toLowerCase() == tagMatches[1]?.toLowerCase());
+		}
+		else {
+			// if any of tag,name,nicknam matches then return the member
+			foundMember = members.find(mem => mem.user.tag == arg || mem.user.username == arg || mem.nickname == arg);
+
+			// if nothing has been found try searching case insensitive
+			arg = arg.toLowerCase();
+			foundMember = foundMember ?? members.find(mem => mem.user.tag.toLowerCase() == arg
+				|| mem.user.username.toLowerCase() == arg || mem.nickname?.toLowerCase() == arg);
+		}
+		return foundMember;
+	}
+	else {
+		// fetch the member, Take the second element since the first is the whole thing
+		const returnMember = await message.guild.members.fetch(idMatches[1])
+			.catch((error) => {
+				console.error(`Error initiated in mention.js: ${error.message}`);
+			});
+		return returnMember;
+
+	}
+}
+
+
+module.exports = {
+	async getMember(message, arg, members) { // if found and optinaly confirmed: returns an object with member, reason
+		if (!arg) return;
+		const member = await fgetMember(message, arg, members); // send a copy
+		const idMatches = arg.match(idregex);
+		const tagMatches = arg.match(tagregex);
+		const simpleName = arg;
+
+
+		// if they used IDy -> then only ID | if they used Metzok#6146yes -> then only tag | nickname: then first arg assuming not spaces in name
+		const inputtedname = idMatches?.[1] ?? tagMatches?.[1] ?? simpleName;
+
+		// only check if member exists
+		let inputType;
+
+		if (idregex.test(arg)) inputType = 'ID'; // they used an ID
+		else if (tagregex.test(arg)) inputType = 'tag'; // they used a tag
+		else inputType = 'name';
+
+		if (!member) { // invalid input
+			message.channel.send({ embed:
+				{ // embed object
+					color: colors.red,
+					description: `Unable to find someone who is boosting the server with the ${inputType}: ${inputtedname}`,
+				},
+			});
+		}
+
+		return member;
+	},
+};
